a08875b5f48653b0df4de3d91fe5c64938a6744b
Tereza <17hlasna@primmat.cz>
2021-04-22 11:21:20 +0200

    hlavicka

--- site/templates/includes/hlavicka.php
+++ site/templates/includes/hlavicka.php
@@ -39,7 +39,7 @@ $slider .= "</li>";
 <html lang="en">
 	<head>
 		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="viewport"  content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
 		<title><?php echo $page->title; ?></title>
 		
 		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.2.0/css/uikit.min.css"/>
