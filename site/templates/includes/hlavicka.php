120cebb174685f9f37b82eb6c66b1108c233013b
Tereza <17hlasna@primmat.cz>
2021-04-22 11:24:34 +0200

    změna

--- site/templates/includes/hlavicka.php
+++ site/templates/includes/hlavicka.php
@@ -36,10 +36,10 @@ $slider .= "</li>";
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="cs">
 	<head>
 		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
-        <meta name="viewport"  content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+        <meta name="viewport" content="width=device-width">
 		<title><?php echo $page->title; ?></title>
 		
 		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.2.0/css/uikit.min.css"/>
